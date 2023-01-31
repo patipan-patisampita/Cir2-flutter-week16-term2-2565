db1613a41d2faf3ea8771b3bfd079d787e0b1e74
patipan-patisampita <patipan.patisampita@gmail.com>
2023-01-31 12:35:32 +0700

    update

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -15,6 +15,27 @@ class _SignUpState extends State<SignUp> {
         centerTitle: true,
         title: Text("ยินดีต้อนรับ สมัครบัญชีผู้ใช้"),
       ),
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    // icon: Icon(Icons.person),
+                    hintText: 'กรุณาป้อนข้อมูลอีเมลล์',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                    suffixIcon: Icon(Icons.check),
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
